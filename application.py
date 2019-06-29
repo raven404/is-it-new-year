b335e2cde8b4a26326aa675003cd084c19d92400
raven404 <gky1232000@gmail.com>
2019-06-29 12:50:22 -0400

    added new route to the file

--- application.py
+++ application.py
@@ -9,3 +9,6 @@ def index():
     now = datetime.datetime.now()
     new_year = now.month == 6 and now.day == 29
     return render_template("index.html", new_year=new_year)
+@app.route("/<string:input>")
+def data():
+    return "Hey Buddy Sorry there is not result for {}!!!".format(name)
