d9cbcb75fd59b54e118963bfeb0d9e3315a71f7d
raven <32195921+raven404@users.noreply.github.com>
2019-06-29 13:00:42 -0400

    improvision in route 1

--- application.py
+++ application.py
@@ -10,5 +10,5 @@ def index():
     new_year = now.month == 6 and now.day == 29
     return render_template("index.html", new_year=new_year)
 @app.route("/<string:input>")
-def data():
-    return "Hey Buddy Sorry there is not result for {}!!!".format(name)
+def data(input):
+    return "Hey Buddy Sorry there is not result for {}!!!".format(input)
